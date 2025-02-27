f7e3cbbbc8b5d765bc3df100c12cf6dab5223fd8
Zemlekop <162012457+JAnderyyy@users.noreply.github.com>
2025-02-27 11:19:27 +0300

    Add files via upload

---
+++ 1задание.py
@@ -0,0 +1,22 @@
+class Car:
+ name = None
+ marka = None
+
+ def right(self):
+     right = None
+    
+ def left(self):
+     left = None
+
+ def down(self):
+     down = None
+ 
+myCar = Car()
+
+myCar.name = 'BOB'
+myCar.marka = 'Kamaz'
+
+myCar.right()
+myCar.left()
+myCar.down()
+ 
