b400b862b91593b3a1b1d71ba1454e4f848ff738
Zemlekop <162012457+JAnderyyy@users.noreply.github.com>
2025-02-27 11:28:11 +0300

    Add files via upload

---
+++ 18задание.py
@@ -0,0 +1,33 @@
+class Employee:
+    __name = None
+    __salary = None
+    __age = None
+    def __init__(self, name, salary, age):
+        self.__name = name
+        self.__salary = salary
+        self.__age = age
+    def getName(self):
+        return self.__name
+    def getSalary(self):
+        return str(self.__salary) + '$'
+    def getAge(self):
+        return self.__age
+    def setName(self, name):
+            self.__name = name
+    def setSalary(self, salary):
+            self.__salary = salary
+    def setAge(self, age):
+        if (age > 0 and age < 120):
+            self.__age = age
+        else:
+            self.__age = 'Нету'
+employee1 = Employee('', '', '')
+employee1.setName('Romanenko')
+employee1.setSalary(200)
+employee1.setAge(17)
+print(employee1.getName())
+print(employee1.getSalary())
+print(employee1.getAge())
+
+
+
